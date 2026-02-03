55abf413cac9a390094edf3e6d2549ae51158322
Justin Beaurivage <code@beaurivage.io>
2026-02-03 12:36:58 -0500

    Add git config+delta pager

--- packages/git/git.nix
+++ packages/git/git.nix
@@ -3,11 +3,45 @@
 {
   home.packages = with pkgs; [
     git
+    delta
   ];
 
   programs.git = {
     enable = true;
-    settings.user.name = "Justin Beaurivage";
-    settings.user.email = "code@beaurivage.io";
+    lfs.enable = true;
+
+    settings = {
+      user = {
+        name = "Justin Beaurivage";
+        email = "code@beaurivage.io";
+      };
+
+      init = {
+        defaultBranch = "main";
+      };
+
+      core = {
+        pager = "delta";
+        editor = "helix";
+      };
+
+      interactive = {
+        diffFilter = "delta --color-only";
+      };
+
+      delta = {
+        side-by-side = true;
+        navigate = true;
+        dark = true;
+      };
+
+      merge = {
+        conflictStyle = "zdiff3";
+      };
+
+      pull = {
+        rebase = true;
+      };
+    };
   };
 }
